36d6ddc5129a767cba1e7cdcc51faea35094011c
shenikar <shenikar@student.21-school.ru>
2024-08-30 13:02:30 +0300

    add hashingWord func

--- service/hash_service.go
+++ service/hash_service.go
@@ -1,2 +1,21 @@
 package service
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+type HashService struct{}
+
+func NewHashService() *HashService {
+	return &HashService{}
+}
+
+func (s *HashService) GetWord(hash string) (string, bool) {
+	return "", false
+}
+
+func (s *HashService) hashingWord(word string) string {
+	hash := md5.Sum([]byte(word))
+	return hex.EncodeToString(hash[:])
+}
