cd3e0ac6560e371d43d6291a3241d6b70d30cea0
shenikar <shenikar@student.21-school.ru>
2024-08-30 16:58:56 +0300

    add func generateWords

--- service/hash_service.go
+++ service/hash_service.go
@@ -12,10 +12,31 @@ func NewHashService() *HashService {
 }
 
 func (s *HashService) GetWord(hash string) (string, bool) {
+	for _, word := range s.generateWords() {
+		if s.hashingWord(word) == hash {
+			return word, true
+		}
+	}
 	return "", false
 }
 
 func (s *HashService) hashingWord(word string) string {
 	hash := md5.Sum([]byte(word))
 	return hex.EncodeToString(hash[:])
+}
+
+func (s *HashService) generateWords() []string {
+	var words [] string
+	chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	// если длина пароля 1 символ
+	for _, ch := range chars {
+		words = append(words, string(ch))
+	}
+	// если длина пароля 2 символ
+	for _, ch1 := range chars {
+		for _, ch2 := range chars {
+			words = append(words, string(ch1)+string(ch2))
+		}
+	}
+	return words
 }
