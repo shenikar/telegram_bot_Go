21f3a4e7f6da6617c396afa57973aeb0cd79dcc5
shenikar <shenikar@student.21-school.ru>
2024-08-31 15:54:24 +0300

    added tests

---
+++ service/hash_service_test.go
@@ -0,0 +1,73 @@
+package service
+
+import (
+	"testing"
+)
+
+// краевые случаи метода GetWord
+func TestGetWordCases(t *testing.T) {
+	service := NewHashService()
+	testCases := []struct {
+		word     string
+		expected string
+		found    bool
+	}{
+		{"", "", false},
+		{"~", "~", true},
+		{"longwordlimit", "", false},
+		{"ñ", "", false},
+		{"nonehash", "", false},
+	}
+	for _, tc := range testCases {
+		hash := service.hashingWord(tc.word)
+		word, found := service.GetWord(hash)
+		if word != tc.expected || found != tc.found {
+			t.Errorf("GetWord(%s) = %v, %v; expected %v,%v", hash, word, found, tc.expected, tc.found)
+		}
+	}
+}
+
+// краевые случаи метода hashingWord
+func TestHashingWordCases(t *testing.T) {
+	service := NewHashService()
+	testCases := []struct {
+		word     string
+		expected string
+	}{
+		{"", service.hashingWord("")},
+		{"~", service.hashingWord("~")},
+		{"longwordlimit", service.hashingWord("longwordlimit")},
+		{"ñ", service.hashingWord("ñ")},
+	}
+	for _, tc := range testCases {
+		hash := service.hashingWord(tc.word)
+		if hash != tc.expected {
+			t.Errorf("hashingWord(%s) = %v; expected %v", tc.word, hash, tc.expected)
+		}
+	}
+}
+
+// краевые случаи метода generateWord
+func TestGenerateWordsCases(t *testing.T) {
+	service := NewHashService()
+	words := service.generateWords()
+	if contains(words, "") {
+		t.Errorf("generateWords() = %v; expected not to contain empty string", words)
+	}
+	if !contains(words, "~") {
+		t.Errorf("generateWords() = %v; expected to contain '~'", words)
+	}
+	if contains(words, "ñ") {
+		t.Errorf("generateWords() = %v; expected not to contain 'ñ'", words)
+	}
+}
+
+// проверка существует ли слово в сгенерированном списке слов
+func contains(s []string, item string) bool {
+	for _, i := range s {
+		if i == item {
+			return true
+		}
+	}
+	return false
+}
