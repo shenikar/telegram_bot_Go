f4dd8b41044ef787e4b5afcaee7a97ee8a4e1931
shenikar <shenikar@student.21-school.ru>
2024-09-11 22:30:41 +0300

    added config file

---
+++ config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Database Databaseconfig
+	Telegram TelegramConfig
+}
+
+type Databaseconfig struct {
+	User     string
+	Password string
+	Name     string
+	Host     string
+	Port     string
+}
+
+type TelegramConfig struct {
+	Token   string
+	Timeout int
+}
+
+func LoadConfig() (*Config, error) {
+	if err := godotenv.Load(".env"); err != nil {
+		return nil, fmt.Errorf("error loading .env file: %v", err)
+	}
+
+	dbConfig := Databaseconfig{
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Name:     os.Getenv("DB_NAME"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+	}
+
+	timeoutStr := os.Getenv("TIMEOUT_BOT")
+	timeout, err := strconv.Atoi(timeoutStr)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing TIMEOUT_BOT: %v", err)
+	}
+
+	telegramConfig := TelegramConfig{
+		Token:   os.Getenv("TELEGRAM_BOT_TOKEN"),
+		Timeout: timeout,
+	}
+
+	return &Config{
+		Database: dbConfig,
+		Telegram: telegramConfig,
+	}, nil
+}
