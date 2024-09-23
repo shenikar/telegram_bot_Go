e9699feb74ccc69eb65990b2b2a366f5ac81e547
shenikar <shenikar@student.21-school.ru>
2024-09-24 00:44:16 +0300

    fix rabbit

--- service/rabbitmq.go
+++ service/rabbitmq.go
@@ -39,24 +39,53 @@ func NewRabbitMqService(amqpURI, queueName string) (*RabbitMQService, error) {
 	}, nil
 }
 
-func (r *RabbitMQService) SendQueue(hash string) error {
+func (r *RabbitMQService) CreateCallbackQueue() (amqp.Queue, error) {
+	return r.channel.QueueDeclare(
+		"",
+		false,
+		true,
+		true,
+		false,
+		nil,
+	)
+}
+
+func (r *RabbitMQService) SendQueueWithReply(hash string, callbackQueueName string, chatID int64) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
 	err := r.channel.PublishWithContext(
-		ctx,          // Передача контекста
-		"",           // Exchange
-		r.queue.Name, // Queue name
-		false,        // Mandatory
-		false,        // Immediate
+		ctx,
+		"",
+		r.queue.Name,
+		false,
+		false,
 		amqp.Publishing{
 			ContentType: "text/plain",
 			Body:        []byte(hash),
+			ReplyTo:     callbackQueueName,
 		},
 	)
 	if err != nil {
 		log.Printf("Failed to publish a message: %v", err)
 		return err
 	}
-	log.Printf(" [x] Sent %s\n", hash)
+	log.Printf(" [x] Sent %s to queue %s\n", hash, r.queue.Name)
 	return nil
+}
+
+func (r *RabbitMQService) ConsumeResults(callbackQueueName string) (<-chan amqp.Delivery, error) {
+	msgs, err := r.channel.Consume(
+		callbackQueueName,
+		"",    // consumer
+		true,  // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil,   // args
+	)
+	if err != nil {
+		return nil, err
+	}
+	return msgs, nil
 }
