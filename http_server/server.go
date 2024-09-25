7ab82ca27533b112bb716efc6fb6a98f1430adba
shenikar <shenikar@student.21-school.ru>
2024-09-25 23:03:24 +0300

    fix serve

--- http_server/server.go
+++ http_server/server.go
@@ -3,7 +3,6 @@ package server
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 	"telegram_bot_go/service"
 )
 
@@ -16,18 +15,12 @@ func NewStatsHandler(statsService *service.StatsService) *StatsHandler {
 }
 
 func (h *StatsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	userIDStr := r.URL.Query().Get("user_id")
-	userID, err := strconv.Atoi(userIDStr)
-	if err != nil {
-		http.Error(w, "Invalid user ID", http.StatusBadRequest)
-		return
-	}
-
-	stats, err := h.statsService.GetStats(userID)
+	stats, err := h.statsService.GetLast24HoursRequests()
 	if err != nil {
 		http.Error(w, "Error retrieving stats", http.StatusInternalServerError)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(stats)
 }
